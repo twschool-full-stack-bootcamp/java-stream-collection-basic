7c0647acbef026382112d8b69e89ddc18849d965
Lin Ning <nlin@thoughtworks.com>
2019-08-23 00:08:20 +0800

    add about map task

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -2,21 +2,22 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.Arrays;
 import java.util.List;
 
-public class Filter {
+public class MyMap {
 
-    List<Integer>  array;
+    List<Integer> array;
 
-    public Filter(List<Integer> array) {
-     this.array = array;
+    public MyMap(List<Integer> array) {
+        this.array = array;
     }
 
-    public List<Integer> filterEven() {
+    public List<Integer> sortFromBig() {
         throw new NotImplementedException();
     }
 
-    public List<Integer> filterMultipleOfThree() {
+    public List<Integer> sortFromSmall() {
         throw new NotImplementedException();
     }
 }
